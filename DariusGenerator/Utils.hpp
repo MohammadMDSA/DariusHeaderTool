dea205534dc74f672663717a966be79af0de1482
Mohammad MDSA <samimd.77@gmail.com>
2023-04-06 15:59:46 +0330

    Add field info helper functions

--- DariusGenerator/Utils.hpp
+++ DariusGenerator/Utils.hpp
@@ -22,3 +22,32 @@ bool IsFieldSerializable(kodgen::FieldInfo const& field)
 			return true;
 	return false;
 }
+
+bool IsResourceField(kodgen::FieldInfo const& field)
+{
+	for (auto const& prop : field.properties)
+		if (prop.name == "Resource")
+			return true;
+	return false;
+}
+
+void GetFieldInfo(kodgen::FieldInfo const& field, bool& isConst, bool& isSerializable, bool& isResource)
+{
+	isSerializable = isResource = false;
+
+	for (auto const& prop : field.properties)
+	{
+		if (prop.name == "Serialize")
+		{
+			isSerializable = true;
+			continue;
+		}
+		else if (prop.name == "Resource")
+		{
+			isResource = true;
+			continue;
+		}
+	}
+
+	isConst = IsFieldConst(field);
+}
